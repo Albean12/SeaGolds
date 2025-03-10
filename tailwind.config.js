b9f42b4d79c907526fec40408a5c4c1f94249410
Albean12 <130741408+Albean12@users.noreply.github.com>
2025-03-10 22:29:03 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Include all components and pages
+    "./public/index.html"          // Include the main HTML file
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
