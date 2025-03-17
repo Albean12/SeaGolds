74c52bccdf005adc807084b49608a5233e9a53d1
Albean12 <alvinguatato9@gmail.com>
2025-03-17 11:11:39 +0800

    Update axiosInstance.js

--- src/pages/Login/axiosInstance.js
+++ src/pages/Login/axiosInstance.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:8000',
-  baseURL: 'https://backend-production-8fda.up.railway.app',  // ✅ Use your deployed backend URL
   withCredentials: true, // Sends cookies with requests
   headers: {
     'Content-Type': 'application/json',
