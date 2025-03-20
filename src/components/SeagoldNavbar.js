eeee30d2d9642b4cb50591da72371334e0999791
Albean12 <alvinguatato9@gmail.com>
2025-03-20 10:09:40 +0800

    Update SeagoldNavbar.js

--- src/components/SeagoldNavbar.js
+++ src/components/SeagoldNavbar.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './SeagoldNavbarCSS.css';
 
-const Navbar = () => {
-  const logoUrl = '/assets/seagoldlogo.jpg'; // ✅ Accessing from your public/assets folder
+const SeagoldNavbar = () => {
+  const logoUrl = `${process.env.REACT_APP_API_URL}/storage/icons/SeagoldLogo.svg`; // Update to your backend logo URL
 
   return (
     <div className="seagold-navbar-container">
       <div className="seagold-navbar">
         <div className="seagold-navbar-logo">
           <img src={logoUrl} alt="Seagold Dormitory Logo" />
-          <span className="seagold-navbar-logo-text">Seagold Dormitory</span>
+          <span className="seagold-navbar-logo-text"></span> {/* Text beside the logo */}
         </div>
         <div className="seagold-navbar-links">
           <Link to="/" className="seagold-navbar-link">Home</Link>
@@ -22,7 +22,7 @@ const Navbar = () => {
         <div className="seagold-navbar-login">
           <Link to="/login" className="seagold-navbar-login-button">
             <span>Account</span>
-            <img src="/assets/user.png" alt="User Icon" />
+            <img src="/user.png" alt="User Icon" />
           </Link>
         </div>
       </div>
@@ -30,4 +30,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default SeagoldNavbar;
